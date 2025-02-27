05fcc1d8140f33325b2d73b2305a13d44f06edc8
Ayat <ayat@MacBooks-MacBook-Pro.local>
2025-02-27 11:24:16 +0600

    prime number list show

--- Prime_Number.py
+++ Prime_Number.py
@@ -12,3 +12,14 @@ def is_prime(n):
 
 result = is_prime(6)
 print(result)
+
+def rang_prime_number(n):
+    primeList = []
+    for i in range(2, n):
+        if is_prime(i):
+            primeList.append(i)
+    return primeList
+        
+
+result2 = rang_prime_number(100)
+print(result2)
